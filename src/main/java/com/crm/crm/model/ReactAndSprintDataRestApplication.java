e0b4d995cd4314adfb1588e9a368f48d67fcadcb
Javier Carreño <carrenojavier015@gmail.com>
2021-02-11 19:28:13 -0800

    Add all my files

---
+++ src/main/java/com/crm/crm/model/ReactAndSprintDataRestApplication.java
@@ -0,0 +1,11 @@
+package com.crm.crm.model;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringApplication;
+
+@SpringBootApplication
+
+public class ReactAndSpringDataRestAplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReactAndSpringDataRestAplication.class, args);
+    }
+}
